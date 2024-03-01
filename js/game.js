134a9b65e2fd256763808f81c8d7c42a054d2982
“rubng87” <gallegoruben_srye@hotmail.com>
2024-03-01 12:43:29 +0100

    juego

--- js/game.js
+++ js/game.js
@@ -5,9 +5,9 @@ let mensajeInicial = `
 PIEDRA, PAPEL I TIJERAS
 =======================
 
-1 <-- Piedra
-2 <-- Papel
-3 <-- Tijeras
+1 <-- ✊
+2 <-- ✋
+3 <-- ✌️
 
 `
 
@@ -15,6 +15,7 @@ let eleccionUsuario = Number(prompt(mensajeInicial))
 console.log("eleccion = ", eleccionUsuario);
 console.log(typeof(eleccionUsuario));
 
+// Elección del usuario
 if (!isNaN(eleccionUsuario)) {
     if (eleccionUsuario == 1) {
     alert("Has elegido ✊");
@@ -25,8 +26,39 @@ if (!isNaN(eleccionUsuario)) {
     } else {
         alert("¡¡ Hasta pronto !!");
     }
+
+    //Elección de la máquina
+    let eleccionPC = getRandomInt(1, 4)
+    if (eleccionPC == 1) {
+        alert("El PC a elegido ✊");
+    } else if (eleccionPC == 2) {
+        alert("El PC a elegido ✋");
+    } else if (eleccionPC == 3) {
+        alert("El PC a elegido ✌️");
+    }
+
+    // Lógica del juego
+    if (eleccionPC == eleccionUsuario) {
+        alert("¡Empate!")
+    } else if ((eleccionUsuario == 1 && eleccionPC == 3) ||
+     (eleccionUsuario == 2 && eleccionPC ==1) ||
+     (eleccionUsuario == 3 && eleccionPC ==2)) {
+        alert("¡Has ganado!")
+    } else {
+        alert("¡Has perdido")
+    }
+
 } else {
     alert("¡¡ Hasta pronto, aprende a elegir en los 3 números !!");
 }
 
 // if (eleccionUsuario == "1" || )
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+// Ejemplo: getRandomInt(1, 4) devolverá 1, 2 o 3 (todos incluidos).
+
+// console.log(getRandomInt(1, 4));
